5bb7c730fb45d1200bf9211b6746a3a6273aa1c9
nedduc <marinacudden@gmail.com>
2021-09-19 11:56:50 +0000

    add user request loop to repeat request for data until care sales data is valid

--- run.py
+++ run.py
@@ -17,22 +17,28 @@ def get_sales_data():
     """
     Get car sales input from user
     """
-    print("Please enter car sales data from the last day of sales")
-    print("Data should be six numbers, seperated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter car sales data from the last day of sales")
+        print("Data should be six numbers, seperated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your car sales here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your car sales here: ")
+
+        sales_data = data_str.split(",")
+
+        if check_data(sales_data):
+            print("Your car sales data is valid!")
+            break
+
+    return sales_data
 
 
-def validate_data(values):
+def check_data(values):
     """
     Inside the try statment, converts all strings values to intergers.
     Raise ValueError is strings can not be converted into intergers,
     or if there is not exactly 6 values entered.
     """
-    print(values)
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -41,6 +47,8 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+    return True
 
 
-get_sales_data()
+data = get_sales_data()
