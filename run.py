c95cbdeddf37ae58d2ea56e6ccd6a5195d11ecfd
nedduc <marinacudden@gmail.com>
2021-09-19 11:28:21 +0000

    Add validation

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('car-sales')
 
+
 def get_sales_data():
     """
     Get car sales input from user
@@ -21,6 +22,25 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your car sales here: ")
-    print(f"The data provided is {data_str}")
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try statment, converts all strings values to intergers.
+    Raise ValueError is strings can not be converted into intergers,
+    or if there is not exactly 6 values entered.
+    """
+    print(values)
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
 
 get_sales_data()
