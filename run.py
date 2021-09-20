eb498339d428d263269a72b9d9ba17b5e6d775fb
nedduc <marinacudden@gmail.com>
2021-09-20 15:14:41 +0000

    Add car sales data to car sales worksheet

--- run.py
+++ run.py
@@ -15,7 +15,10 @@ SHEET = GSPREAD_CLIENT.open('car-sales')
 
 def get_sales_data():
     """
-    Get car sales input from user
+    Get car sales input from user.
+    Run a while loop ro collect a valid string of data from user
+    via the terminal, a string of 6 numbers seperated by commas.
+    The loope will repeat request, until valid.
     """
     while True:
         print("Please enter car sales data from the last day of sales")
@@ -48,7 +51,20 @@ def check_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
+
     return True
 
 
+def update_sales_worksheet(data):
+    """
+    Update worksheet for car sales, add new row when the list data provided.
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
