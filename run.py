59c9a28b30027c6dc31336e798e809b721b7d390
nedduc <marinacudden@gmail.com>
2021-10-03 12:20:50 +0000

    comments added

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+"""
+code for this project was based on love-sandwiches tutorial
+https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+LS101+2021_T1/courseware/293ee9d8ff3542d3b877137ed81b9a5b/071036790a5642f9a6f004f9888b6a45/?child=first
+"""
 import gspread
 from google.oauth2.service_account import Credentials
 import json
